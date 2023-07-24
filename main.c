5a133501eac238c9ef845ff246598d6ebc640b99
d0pelrh <jsshscifi@gmail.com>
2023-07-24 16:49:48 +0200

    cursor sweeps the window in a given fps

--- main.c
+++ main.c
@@ -1,20 +1,54 @@
 #include <ncurses.h>
+#include <stdlib.h> //malloc
+#include <time.h> //CLOCKS_PER_SEC clock
+#include <unistd.h> //usleep
+
+#include "struct.h"
 
 int	main(int ac, char **av){
 initscr(); // = NCURSES INIT ===
 cbreak();
 noecho();
 //nodelay(stdscr, TRUE);
-curs_set(0); // ================
+//curs_set(0); // ================
+WINDOW	*win;
+struct curs	curs;
+struct ptng	ptng;
+clock_t	clk_start, clk;
+int	fps, refresh_rate;
+
+curs.i = 0; curs.c = '.';
+ptng.w = 70; ptng.h = 20;
+ptng.buf = (int *)malloc(70*20*sizeof(int));
+for (int i=0; i<70*20; i++)
+	ptng.buf[i] = 0;
+fps = 5;
+refresh_rate = CLOCKS_PER_SEC/fps;
 
 refresh();
-WINDOW	*win = newwin(22, 72, 3, 7);
+win = newwin(22, 72, 3, 7);
 box(win, 0, 0);
 wrefresh(win);
 getch();
 delwin(win);
 
 win = newwin(20, 70, 4, 8);
+
+clk_start = clock(); while (1){
+
+wprintw(win, "x"); curs.i++;
+
+if (curs.i == ptng.w*ptng.h){
+	curs.i = 0;
+	wmove(win, 0, 0);}
+
+wrefresh(win);
+
+clk = clock() - clk_start;
+if (clk < refresh_rate)
+	usleep((refresh_rate-clk)*(1000000/CLOCKS_PER_SEC));
+clk_start = clock();}
+
 
 
 delwin(win);
---
+++ struct.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+struct	curs{
+	int	i;
+	char	c;
+	int	s;
+};
+
+struct	ptng{
+	short	w;
+	short	h;
+	int	*buf;
+};
+
+#endif
